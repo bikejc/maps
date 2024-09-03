d73ec86b3c266fb3e19ceb312681da9246b6d67c
Ryan Williams <ryan@runsascoded.com>
2024-09-03 17:22:13 -0400

    update pbl map

--- download-map.py
+++ download-map.py
@@ -6,12 +6,14 @@ from os.path import dirname, exists
 import click
 import json
 from requests import get
+from utz import err
 
 
 # https://services2.arcgis.com/UXbywc7dSkfgdPp4/ArcGIS/rest/services/Jersey_City_Bike_Network/FeatureServer/55/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&relationParam=&returnGeodetic=false&outFields=*&returnGeometry=true&returnEnvelope=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=4326&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=
 @click.command()
+@click.option('-f', '--overwrite', is_flag=True)
 @click.argument('service')
-def main(service):
+def main(overwrite, service):
     format = "json"
     server = "UXbywc7dSkfgdPp4"
     host = "https://services2.arcgis.com"
@@ -25,11 +27,14 @@ def main(service):
         name = layer['name']
         outpath = f'public/{service}/{name}.json'
         if exists(outpath):
-            print(f'Found {outpath}; skipping')
-            continue
+            if overwrite:
+                err(f'Found {outpath}; overwriting')
+            else:
+                err(f'Found {outpath}; skipping')
+                continue
         makedirs(dirname(outpath), exist_ok=True)
         url = f"{feature_server_url}/{layer_id}/query?f={format}&where=1%3D1&returnGeometry=true&outFields=*&outSR=4326"
-        print(f'Fetching {url} to {outpath}')
+        err(f'Fetching {url} to {outpath}')
         resp = get(url).json()
         with open(outpath, 'w') as f:
             f.write(json.dumps(resp, indent=4))
